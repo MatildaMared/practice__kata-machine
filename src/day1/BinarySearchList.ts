ecad97818e200df85c14d6468d7a56bdc0f8d153
Matilda Mared <matildamared@live.se>
2022-09-28 13:12:38 +0200

    solve binear search list

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,20 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0;
+    let high = haystack.length;
 
+    do {
+        let middle = Math.floor(low + (high - low) / 2);
+
+        if (haystack[middle] === needle) {
+            return true;
+        }
+
+        if (needle > haystack[middle]) {
+            low = middle + 1;
+        } else {
+            high = middle;
+        }
+    } while (low < high);
+
+    return false;
 }
